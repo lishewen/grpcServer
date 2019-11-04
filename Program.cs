7caaee1e501b63ddc1cefa78dc15005df997dc5e
算神 <lishewen@msn.com>
2019-11-04 09:38:51 +0800

    发布到Linux

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
@@ -17,9 +18,17 @@ namespace dotnetcore3
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSystemd()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.ConfigureKestrel(options =>
+                    {
+                        options.Listen(IPAddress.Loopback, 5002, listenOptions =>
+                        {
+                            listenOptions.UseHttps("openssl.pfx", "123456");
+                        });
+                    })
+                    .UseStartup<Startup>();
                 });
     }
 }
